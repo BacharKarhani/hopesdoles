877fed06fa09431278f64dedb256a57ae47a32bf
BacharKarhani <bacharkarhani0@gmail.com>
2025-02-13 14:08:18 +0200

    //

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,7 +54,7 @@ export default function Header() {
               href="/"
               rel="noopener noreferrer"
             >
-              <img src={logo} width={100} height={50} alt="Logo" />
+              <img src={logo} width={80} height={50} alt="Logo" />
             </a>
           </div>
 
