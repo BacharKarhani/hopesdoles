55b78852e61c59c642568bf2d5e05e260f3298f6
BacharKarhani <bacharkarhani0@gmail.com>
2025-02-14 09:58:47 +0200

    animation for dropdown in mobile view

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,47 +1,36 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import "./Header.css";
 import { FiAlignJustify } from "react-icons/fi";
+import "./Header.css";
 
 function DropDown() {
-  // const [open, setOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <div className="dropdown">
-        <FiAlignJustify
-          className="dropbtn"
-          sx={{ fontSize: 32 }}
-          onClick={() => {
-            setIsOpen(!isOpen); // toggle
-          }}
-        />
-        {isOpen && (
-          <div className="dropdown-content">
-            {/* <div className="dropdown-image">
-          <img src={logo} alt="hopes"></img>
-        </div> */}
-            <Link className="links" to="/">
-              Home
-            </Link>
-            <Link className="links" to="/story">
-              Story
-            </Link>
-            <Link className="links" to="/dolls">
-              Dolls
-            </Link>
-            <Link className="links" to="/hoops">
-              Hoops
-            </Link>
-            <Link className="links" to="/facts/PsychologicalFacts">
-              Psychological Facts
-            </Link>
-          </div>
-        )}
+    <div className="dropdown">
+      <FiAlignJustify
+        className="dropbtn"
+        sx={{ fontSize: 32 }}
+        onClick={() => setIsOpen(!isOpen)}
+      />
+      <div className={`dropdown-content ${isOpen ? "show" : ""}`}>
+        <Link className="links" to="/">
+          Home
+        </Link>
+        <Link className="links" to="/story">
+          Story
+        </Link>
+        <Link className="links" to="/dolls">
+          Dolls
+        </Link>
+        <Link className="links" to="/hoops">
+          Hoops
+        </Link>
+        <Link className="links" to="/facts/PsychologicalFacts">
+          Psychological Facts
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
 
